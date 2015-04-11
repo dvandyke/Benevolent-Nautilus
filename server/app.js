6fa0099693485f5dd14d71fa718dd798f47ccb7b
Derek Van Dyke <haoled@gmail.com>
2015-04-10 17:22:28 -0700

    Basic router inserted into app

--- server/app.js
+++ server/app.js
@@ -1,14 +1,24 @@
 /**
 @fileOverview 
 
-<p>Server initial file/p>
+<p>Server initial file</p>
 
 <p>Open up a port to listen for requests</p>
 
 */
 var express = require('express');
+var path = require('path');
+
+
 var app = express();
 var port = 8080;
+
+app.set('views', path.join(__dirname, '../client/dist/'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.engine('html', require('ejs').renderFile);
 
 app.listen(port);
 console.log('Listening on port: ', port);
